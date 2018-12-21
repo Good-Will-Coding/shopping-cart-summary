2152aac7fa4343bd83bc0963ebf587fee11daf41
billy <billyliadphillips@gmail.com>
2018-12-21 13:51:15 +0000

    Added estimatedTotal and imported react-bs to taxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
-import SubTotal from './components/Subtotal/Subtotal';
-import PickupSavings from './components/PickupSavings/PickupSavings';
-import TaxesFees from './components/TaxesFees/TaxesFees';
-import './App.css';
+import React, { Component } from "react";
+import { Grid } from "react-bootstrap";
+import SubTotal from "./components/Subtotal/Subtotal";
+import PickupSavings from "./components/PickupSavings/PickupSavings";
+import TaxesFees from "./components/TaxesFees/TaxesFees";
+import EstimatedTotal from './components/EstimatedTotal/EstimatedTotal';
+import "./App.css";
 
 class App extends Component {
   state = {
     total: 100,
     PickupSavings: -3.85,
-    taxes: 0
-  }
+    taxes: 0,
+    estimatedTotal: 0
+  };
 
   render() {
     return (
       <div className="container">
-      <Grid className="purchase-card">
-      <SubTotal price={this.state.total.toFixed(2)} />
-      <PickupSavings price={this.state.PickupSavings} />
-      <TaxesFees taxes={this.state.taxes.toFixed(2)} />
-      </Grid>
+        <Grid className="purchase-card">
+          <SubTotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.PickupSavings} />
+          <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+          <hr />
+          <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
+        </Grid>
       </div>
     );
   }
